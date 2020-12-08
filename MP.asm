8e55b79a99000defe7c2b336f5a0922e855c16ed
Beaverly Cuasi <61379865+recklessbeav@users.noreply.github.com>
2020-12-09 01:35:30 +0800

    Update MP.asm

--- MP.asm
+++ MP.asm
@@ -4,8 +4,8 @@ section .data
 
 rA db 2
 cA db 3
-matrixA db 0,1,3
-        db 5,7,8
+matrixA db 2,3,4
+        db 1,0,0
         
 rB db 3
 cB db 2
@@ -15,6 +15,8 @@ matrixB db 0,127
         
 row db 0
 col db 0
+i   db 0
+temp dw 0
 
 section .text
 global CMAIN
@@ -24,50 +26,53 @@ CMAIN:
 LOOP3:
 
     ;matrixA
-    MOV EAX, [row]
+    MOV AL, [row] ;0
+    IMUL byte [cA] ; 0
+    MOV DL, [i] ;PALIT ;0
+    MOVSX DX, DL ;SIGN EXTENSION ;P
+    ADD AX, DX ;0
     
-    i = 0
-    row * cA + i
-    
-    MOV AL, byte [matrixA + EAX]
-    PRINT_DEC 1, AL
-    
-    NEWLINE
-    
+    MOVSX EAX, AX  ;SIGN PARA MALAGAY SA 
+    MOV CL, byte [matrixA + EAX] ;0
+   
     ;matrixB
-    MOV EBX, i
-    
-    cB * i + col ; col will implement if LOOP3 is done | L3 limit = cA
-    
-    MOV BL, byte [matrixB + EBX]
-    PRINT_DEC 1, BL
-    INC i
-    CMP i, cA
+    MOV AL, [i] ; RECYCLE ;P
+    IMUL byte [cB]
+    MOV DH, [col]
+    MOVSX SI, DH ;P
+    ADD AX, SI
+    MOVSX EBX, AX 
+    MOV DL, byte [matrixB + EBX] ;P
+    MOVSX CX, CL
+    MOVSX DX, DL
+    IMUL CX,DX
+    ADD [temp], CX
+    INC byte [i]
+    MOV DH,[i] ;P
+    CMP DH, [cA]
     JE LOOP2
     JMP LOOP3
     
-    NEWLINE
-    
-    ;PRINT
-    IMUL BL
-    ;ADD TEMP, AX
-    PRINT_DEC 1, AX
-    JMP END
-    
 LOOP2:
-    i = 0
-    ;TEMP ACCUMULATE OF WHAT YOU MULTIPLIED    
-    INC col
-    CMP col, cB
+     NEWLINE
+     PRINT_DEC 2, temp 
+     PRINT_CHAR " "
+     MOV dword [temp],0 ; CAUTION NA MAS MALAKI
+     MOV byte [i],0
+    INC byte [col]
+    MOV DH, [col] ;P
+    CMP DH, [cB]
     JE LOOP1
     JMP LOOP3
-    ;PRINT
     
 LOOP1:
-    col = 0
-    INC row
-    CMP rA, row
-    JNE L3
+    MOV byte [i],0
+    MOV byte [col],0
+    INC byte [row]
+    MOV CH, [row]
+    CMP CH, [rA]
+    JNE LOOP3
+    JMP END
     
 END:
     NOP    
